2a56a040af7d2c18b5510d4881408e440546332b
John Doe <johndoe@example.com>
2023-09-18 14:28:57 +0200

    writing script for date week and fixing responsive bug

--- script.js
+++ script.js
@@ -13,7 +13,10 @@ document.addEventListener("DOMContentLoaded", function () {
   const dayOfWeek = daysOfWeek[today.getDay()];
   date.textContent = `${dayOfWeek}`;
 
+ 
+
   const weatherDataElement = document.getElementById("weather");
+  const weekDay = document.getElementById("weekDay");
 
   const apiUrl = `https://api.weatherapi.com/v1/current.json?key=190e2717cf5f408e890130520231109&q=London&aqi=yes`;
 
@@ -29,7 +32,17 @@ document.addEventListener("DOMContentLoaded", function () {
     .then((data) => {
       const temperature = data.current.temp_c;
       const condition = data.current.condition.text;
+      const week = data.location.localtime;
+
+      const apiDate = new Date(week);
+      const year = apiDate.getFullYear();
+      const month = apiDate.getMonth() + 1;
+      const day = apiDate.getDate();
+      const formattedDate = `${year}-${month.toString().padStart(2, "0")}-${day
+        .toString()
+        .padStart(2, "0")}`;
       weatherDataElement.textContent = `Temperature: ${temperature}°C, Condition: ${condition}`;
+      weekDay.textContent = formattedDate;
     })
     .catch((error) => {
       console.error("Fetch error:", error);
